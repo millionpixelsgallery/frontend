c575b010bfe9c22536f2fc51974c22e26cb87950
taras <yevchyk5@gmail.com>
2021-05-20 16:28:20 +0300

    feat: add Row, Col

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,9 +1,18 @@
 import px from 'utils/style/px'
+import when from 'utils/style/when'
+import pxToRem from 'utils/style/pxToRem'
 
 const theme = {
-  component: {},
+  component: {
+    NavLink: {
+      activeLine: '#5E72EB',
+      hoverLine: '#8d9cea',
+    },
+  },
   color: {},
   px,
+  pxToRem,
+  when,
 }
 
 export default theme
