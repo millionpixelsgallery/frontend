ee1914a5f767825ba6bf2d8bf8944c8bcca0bf55
taras <yevchyk5@gmail.com>
2021-05-25 16:33:09 +0300

    feat: add no pixels component

---
+++ src/components/NoPixels/styled.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export interface NoPixelsSCProps {}
+
+export const NoPixelsSC = styled.div<NoPixelsSCProps>`
+  box-shadow: 0 16px 40px #0b074233;
+  width: 1000px;
+  background-color: #ffffff;
+  padding: 60px 0 50px;
+`
