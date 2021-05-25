69838f6d9a98cb6fc7f28cef1563b95b6fc54ced
taras <yevchyk5@gmail.com>
2021-05-25 16:50:07 +0300

    feat: return app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import NoPixels from 'components/NoPixels'
+import Canvas from 'components/ui/Canvas'
+import Grid from 'components/ui/Canvas/Grid'
+import Viewport from 'components/ui/Canvas/Viewport'
+import { useCallback, useState } from 'react'
+import Rectangle from 'components/ui/Canvas/Rectangle'
+import useStatic from 'hooks/useStatic'
 
 function App() {
-  return <NoPixels />
+  const [gridVisible, setGridVisible] = useState(false)
+  return (
+    <Canvas width={1000} height={1000} options={useStatic({ backgroundColor: 0xf8f8f8 })}>
+      <Viewport onZoomed={useCallback((e) => setGridVisible(e.viewport.scaled >= 4), [])}>
+        <Rectangle x={300} y={300} width={400} height={400} color={0xeaeaea} />
+        <Rectangle x={450} y={450} width={100} height={100} color={0xdddddd} />
+        <Grid
+          gap={1}
+          lineWidth={0.1}
+          color={0xececec}
+          width={1000}
+          height={1000}
+          hidden={!gridVisible}
+        />
+      </Viewport>
+    </Canvas>
+  )
 }
 
 export default App
