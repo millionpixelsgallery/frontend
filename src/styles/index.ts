8fe2c9d770f33674c854855c13452c489f306106
Vladyslav <vlad.rasenko@gmail.com>
2021-05-26 13:09:09 +0300

    [fix] font import

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,23 +1,17 @@
 import { createGlobalStyle } from 'styled-components'
+import './font.css'
 
 export default createGlobalStyle`
-  
-  @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&display=swap');
-
-  @font-face {
-    font-family: 'RESIN';
-    src: local('RESIN'), url('./fonts/RESIN.woff') format('woff');
-    font-display: swap;
-  }
-
-  
   body {
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
-    'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    padding: 100px;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    padding: 100px;
+  }
+  
+  body, input, label {
+    font-family: 'Oswald', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
+    'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
   }
 
   code {
