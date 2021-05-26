b6020803979c3d0be0e31defc30f262a27d7d35c
@pavlova.vera18 <pavlova.vera18@gmail.com>
2021-05-26 23:00:48 +0300

    feat: add sidebar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,6 +1,9 @@
 import { CSSProperties, memo } from 'react'
 import { SideBarSC, SideBarSCProps } from './styled'
 import { Col, Row } from 'components/ui/Grid'
+import Text from 'components/ui/Text'
+import { margin, marginRight, marginTop } from 'utils/style/indents'
+import SocialIcon from 'components/ui/SocialIcon'
 
 export interface SideBarProps extends SideBarSCProps {
   className?: string
@@ -10,8 +13,63 @@ export interface SideBarProps extends SideBarSCProps {
 function SideBar({ className, style, ...rest }: SideBarProps) {
   return (
     <SideBarSC className={className} style={style} {...rest}>
-      <Col>
-        sidebar1
+      <Col style={margin(173, 0, 0, 50)}>
+        <Text>
+          <div>
+            PIXELS SOLD
+          </div>
+          <div>
+            145,915
+          </div>
+          <div style={marginTop(50)}>
+            NFT CREATED
+          </div>
+          <div>
+            977
+          </div>
+        </Text>
+        <Row>
+          <Col>
+            <Text>1px</Text>
+            <Row>
+              <Text>0.001</Text>
+              <Col>
+                <div className={'burger'}></div>
+                <div className={'burger'}></div>
+                <div className={'burger'}></div>
+              </Col>
+            </Row>
+          </Col>
+          <img src='/assets/Component3.svg'/>
+        </Row>
+        <Row>
+          <Col>
+            <Text>1px</Text>
+            <Row>
+              <Text>0.001</Text>
+              <Col>
+                <div className={'burger'}></div>
+                <div className={'burger'}></div>
+                <div className={'burger'}></div>
+              </Col>
+            </Row>
+          </Col>
+          <img src='/assets/Component4.svg'/>
+        </Row>
+        <Row>
+          <Col>
+            <Text>1px</Text>
+            <Row>
+              <Text>0.001</Text>
+              <Col>
+                <div className={'burger'}></div>
+                <div className={'burger'}></div>
+                <div className={'burger'}></div>
+              </Col>
+            </Row>
+          </Col>
+          <img src='/assets/Component5.svg'/>
+        </Row>
       </Col>
     </SideBarSC>
   )
--- src/components/SideBar/styled.tsx
+++ src/components/SideBar/styled.tsx
@@ -2,4 +2,11 @@ import styled from 'styled-components'
 
 export interface SideBarSCProps {}
 
-export const SideBarSC = styled.div<SideBarSCProps>``
+export const SideBarSC = styled.div<SideBarSCProps>`
+  .burger {    
+    width: 9px;
+    height: 2px;
+    background-color: #5E72EB;
+    margin: 1px 0;    
+  }
+`
