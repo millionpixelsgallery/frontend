7712777cebb41ba5f6d26cd97c378bb6927de94f
Yaroslav Pavlov <pixelycia@gmail.com>
2021-06-08 12:17:40 +0300

    [feature]: update contract

--- server/contracts/Vault.sol
+++ server/contracts/Vault.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
 contract Vault is ERC20PresetFixedSupply {
     address[] founders;
 
-    bool active = true;
+    bool active;
 
     uint256 public accumulatedEarnings;
     uint256 public lastBalance;
@@ -15,21 +15,21 @@ contract Vault is ERC20PresetFixedSupply {
     mapping(address => uint256) founderDebt;
 
     constructor(address[] memory _founders, uint256[] memory _shares)
-    ERC20PresetFixedSupply(
-        "MillionPixelsGallery",
-        "MPG",
-        10000000 * 1 ether,
-        _msgSender()
-    )
+        ERC20PresetFixedSupply(
+            "MillionPixelsGallery",
+            "MPG",
+            10000000 * 1 ether,
+            _msgSender()
+        )
     {
+        active = true;
         require(_founders.length == _shares.length, "invalid data length");
         for (uint8 i = 0; i < _founders.length; i++) {
             transfer(_founders[i], _shares[i] * 1 ether);
         }
         require(totalSupply() == 10000000 * 1 ether, "not 100% shares");
         founders = _founders;
-        buyPremium = 10500;
-        // 105%
+        buyPremium = 10500; //105%
     }
 
     receive() external payable {}
@@ -40,6 +40,7 @@ contract Vault is ERC20PresetFixedSupply {
         uint256 amount
     ) internal virtual override {
         super._beforeTokenTransfer(from, to, amount);
+
         if (from != address(0)) {
             // regular transfer
             require(active, "shares not active");
@@ -50,8 +51,12 @@ contract Vault is ERC20PresetFixedSupply {
         require(active == false, "shares has been activated");
 
         accumulatedEarnings += payable(address(this)).balance - lastBalance;
-        uint256 founderShare = (balanceOf(_msgSender()) * accumulatedEarnings) / totalSupply();
-        require(founderShare >= value + founderDebt[_msgSender()], "value not available");
+        uint256 founderShare =
+            (balanceOf(_msgSender()) * accumulatedEarnings) / totalSupply();
+        require(
+            founderShare >= value + founderDebt[_msgSender()],
+            "value not available"
+        );
         founderDebt[_msgSender()] += value;
         lastBalance = payable(address(this)).balance;
     }
@@ -59,7 +64,8 @@ contract Vault is ERC20PresetFixedSupply {
     function sell(uint256 amount) external {
         require(active, "shares not active");
         require(balanceOf(_msgSender()) >= amount, "not enough balance");
-        uint256 share = (payable(address(this)).balance * amount) / totalSupply();
+        uint256 share =
+            (payable(address(this)).balance * amount) / totalSupply();
         require(share > 0, "not enough in vault");
         _burn(_msgSender(), amount);
         payable(_msgSender()).transfer(share);
@@ -67,7 +73,9 @@ contract Vault is ERC20PresetFixedSupply {
 
     function buy(uint256 amount) external payable {
         require(active, "shares not active");
-        uint256 share = (payable(address(this)).balance * amount * buyPremium) / (totalSupply() * 10000);
+        uint256 share =
+            (payable(address(this)).balance * amount * buyPremium) /
+                (totalSupply() * 10000);
         require(share <= msg.value, "payment too low");
         _mint(_msgSender(), amount);
     }
